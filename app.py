e7d9f0575d9d621193fea45f2d36ac3d50e5d805
Muhammed Uluel <mhmmduluel@gmail.com>
2022-11-22 19:25:08 +0300

    Bug fixes

--- app.py
+++ app.py
@@ -7,41 +7,45 @@ import postSchema_pb2 as pb # the file that compilet from '.proto'
 
 # Definitions #
 TOKEN = sys.argv[1]
-keyword = sys.argv[2]
-limit = sys.argv[3]
-after =''
-respond = ''
+KEYWORD = sys.argv[2]
+LIMIT = sys.argv[3]
+AFTER = sys.argv[4]
+HEADERS = {**getToken.headers, **{"Authorization": f"bearer {TOKEN}"}}
+
 postList = pb.PostList()
-p = postList.posts.add()
-headers = {**getToken.headers, **{"Authorization": f"bearer {TOKEN}"}}
 
-# Adding data to Proto Post model
-def promptForPost(p, post):
-    p.name = post['name']
-    p.subreddit = post['subreddit']
-    p.author_fullname = post['author_fullname']
-    p.title = post['title']
-    p.subreddit_name_prefixed = post['subreddit_name_prefixed']
-    p.ups = post['ups']
-    p.thumbnail = post['thumbnail']
-    p.created = post['created']
-    p.subreddit_id = post['subreddit_id']
-    p.id = post['id']
-    p.url = post['url']
-    p.num_comments = post['num_comments']
-    p.total_awards_received = post['total_awards_received']
+try:
+    with open('posts', 'ab+') as file:
+        respond = ''
+        while respond != "q":
+            # Request
+            result = requests.get("https://oauth.reddit.com/search", 
+                                    headers = HEADERS,
+                                    params={'q':KEYWORD,'limit':LIMIT,'after':AFTER}
+                                )
+            for item in result.json()['data']['children']:
+                post = item['data']
+                # After first request takes last post name. 
+                AFTER = post['name']
+                # Declare Post model
+                p = postList.posts.add()
+                p.name = post['name']
+                p.subreddit = post['subreddit']
+                p.author_fullname = post['author_fullname']
+                p.title = post['title']
+                p.subreddit_name_prefixed = post['subreddit_name_prefixed']
+                p.ups = post['ups']
+                p.thumbnail = post['thumbnail']
+                p.created = post['created']
+                p.subreddit_id = post['subreddit_id']
+                p.id = post['id']
+                p.url = post['url']
+                p.num_comments = post['num_comments']
+                p.total_awards_received = post['total_awards_received']
+                
+            respond = input("Press 'q' for quit:")
 
-with open('posts', 'wb+') as file:
-    while respond != "q":
-        response = requests.get("https://oauth.reddit.com/search", 
-                                headers = headers,
-                                params={'q':keyword,'limit':limit,'after':after}
-                            )
-        for item in response.json()['data']['children']:
-            post_details = item['data']
-            # After first request takes last post name. 
-            after = post_details['name']
-            promptForPost(p,post_details)
-          
         file.write(postList.SerializeToString())
-        respond = input("Press 'q' for quit:")
+
+except Exception as e:
+    print(f"Error occurred: {e}. \n Program is closing...")
