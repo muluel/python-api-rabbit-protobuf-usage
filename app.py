a865337d3f9477b84e7e19662b3d855de9e4d768
Muhammed Uluel <mhmmduluel@gmail.com>
2022-11-22 18:10:43 +0300

    response saved using Proto model

--- app.py
+++ app.py
@@ -3,50 +3,20 @@ import requests.auth
 import json
 import sys
 import getToken
-import postSchema_pb2 as pb # Protoc dosyasından üretilen paketi ekledik
+import postSchema_pb2 as pb # the file that compilet from '.proto'
 
+# Definitions #
 TOKEN = sys.argv[1]
 keyword = sys.argv[2]
 limit = sys.argv[3]
-after=''
-respond= ''
-
+after =''
+respond = ''
+postList = pb.PostList()
+p = postList.posts.add()
 headers = {**getToken.headers, **{"Authorization": f"bearer {TOKEN}"}}
 
-with open('posts.json', 'w+', encoding='utf-8') as file:
-    while respond != "q":
-        response = requests.get("https://oauth.reddit.com/search", 
-                                headers = headers,
-                                params={'q':keyword,'limit':limit,'after':after}
-                            )
-        json_object = {
-            'posts':[]
-        }
-        for item in response.json()['data']['children']:
-            post = item['data']
-            after = post['name']
-            json_object['posts'].append({
-                    'name': post['name'],
-                    'subreddit': post['subreddit'],
-                    'author_fullname': post['author_fullname'],
-                    'title': post['title'],
-                    'subreddit_name_prefixed': post['subreddit_name_prefixed'],
-                    'ups': post['ups'],
-                    'thumbnail': post['thumbnail'],
-                    'created': post['created'],
-                    'subreddit_id': post['subreddit_id'],
-                    'id': post['id'],
-                    'url': post['url'],
-                    'num_comments': post['num_comments'],
-                    'total_awards_received': post['total_awards_received']
-                })
-            print(post['title'])
-        
-        file.write(json.dumps(json_object,indent=2))
-        respond = input("Press 'q' for quit:")
-
-
-def promptForPost(p,post):
+# Adding data to Proto Post model
+def promptForPost(p, post):
     p.name = post['name']
     p.subreddit = post['subreddit']
     p.author_fullname = post['author_fullname']
@@ -61,10 +31,17 @@ def promptForPost(p,post):
     p.num_comments = post['num_comments']
     p.total_awards_received = post['total_awards_received']
 
-pL = pb.PostList()
-for post in json_object['posts']:
-    p = pL.posts.add()
-    promptForPost(p,post)
-
-for post in pL.posts:
-    print(post)
+with open('posts', 'wb+') as file:
+    while respond != "q":
+        response = requests.get("https://oauth.reddit.com/search", 
+                                headers = headers,
+                                params={'q':keyword,'limit':limit,'after':after}
+                            )
+        for item in response.json()['data']['children']:
+            post_details = item['data']
+            # After first request takes last post name. 
+            after = post_details['name']
+            promptForPost(p,post_details)
+          
+        file.write(postList.SerializeToString())
+        respond = input("Press 'q' for quit:")
