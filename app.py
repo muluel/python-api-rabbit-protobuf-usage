60fb85a38b21ce4ea597b34ec85d9d5bc0108414
Muhammed Uluel <mhmmduluel@gmail.com>
2022-11-22 17:21:29 +0300

    protobuf added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests.auth
 import json
 import sys
 import getToken
+import postSchema_pb2 as pb # Protoc dosyasından üretilen paketi ekledik
 
 TOKEN = sys.argv[1]
 keyword = sys.argv[2]
@@ -18,11 +19,13 @@ with open('posts.json', 'w+', encoding='utf-8') as file:
                                 headers = headers,
                                 params={'q':keyword,'limit':limit,'after':after}
                             )
-        json_object={}
+        json_object = {
+            'posts':[]
+        }
         for item in response.json()['data']['children']:
             post = item['data']
             after = post['name']
-            json_object[post['name']] = {
+            json_object['posts'].append({
                     'name': post['name'],
                     'subreddit': post['subreddit'],
                     'author_fullname': post['author_fullname'],
@@ -36,9 +39,32 @@ with open('posts.json', 'w+', encoding='utf-8') as file:
                     'url': post['url'],
                     'num_comments': post['num_comments'],
                     'total_awards_received': post['total_awards_received']
-                }
+                })
             print(post['title'])
+        
+        file.write(json.dumps(json_object,indent=2))
+        respond = input("Press 'q' for quit:")
 
-        json_object = json.dumps(json_object,indent=2)
-        file.write(json_object)
-        respond=input("Press 'q' for quit:")
+
+def promptForPost(p,post):
+    p.name = post['name']
+    p.subreddit = post['subreddit']
+    p.author_fullname = post['author_fullname']
+    p.title = post['title']
+    p.subreddit_name_prefixed = post['subreddit_name_prefixed']
+    p.ups = post['ups']
+    p.thumbnail = post['thumbnail']
+    p.created = post['created']
+    p.subreddit_id = post['subreddit_id']
+    p.id = post['id']
+    p.url = post['url']
+    p.num_comments = post['num_comments']
+    p.total_awards_received = post['total_awards_received']
+
+pL = pb.PostList()
+for post in json_object['posts']:
+    p = pL.posts.add()
+    promptForPost(p,post)
+
+for post in pL.posts:
+    print(post)
